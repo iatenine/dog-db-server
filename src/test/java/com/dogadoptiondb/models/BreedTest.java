450e47ad11b29cae39db642a9bd60f9873ab9a4e
dscanga33 <dscanga33@gmail.com>
2022-02-11 12:41:50 -0500

    Added tests for User and Breed

---
+++ src/test/java/com/dogadoptiondb/models/BreedTest.java
@@ -0,0 +1,56 @@
+package com.dogadoptiondb.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BreedTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getAverageWeight() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void setDescription() {
+    }
+
+    @Test
+    void setAverageWeight() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void canEqual() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
