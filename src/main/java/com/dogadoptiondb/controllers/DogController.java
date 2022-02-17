c5faff64e01c53fbfdb8c44e9d4dfc179f30f7dc
Tanman <tknielsen32@gmail.com>
2022-02-17 14:06:08 -0600

    For review

--- src/main/java/com/dogadoptiondb/controllers/DogController.java
+++ src/main/java/com/dogadoptiondb/controllers/DogController.java
@@ -5,15 +5,13 @@ import com.dogadoptiondb.services.DogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
 @RestController
+@CrossOrigin
 public class DogController
 {
     DogService ds;
